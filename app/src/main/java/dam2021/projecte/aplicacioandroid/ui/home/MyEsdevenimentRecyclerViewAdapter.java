58748145dc02fedffc0707e54a5608d4a3eb4bbb
Javier <xgarciab88@gmail.com>
2021-05-01 02:21:33 +0200

    Navegació desde clic a card esdeveniments a recyclerview d'activitats

--- app/src/main/java/dam2021/projecte/aplicacioandroid/ui/home/MyEsdevenimentRecyclerViewAdapter.java
+++ app/src/main/java/dam2021/projecte/aplicacioandroid/ui/home/MyEsdevenimentRecyclerViewAdapter.java
@@ -58,7 +58,7 @@ public class MyEsdevenimentRecyclerViewAdapter extends RecyclerView.Adapter<MyEs
                 String nom = ((TextView) v.findViewById(R.id.details)).getText().toString();
                 data.putString("id", nom);
                 Navigation.findNavController(v)
-                        .navigate(R.id.action_navigation_home_to_homeFragment, data);
+                        .navigate(R.id.action_navigation_home_to_activitatFragment, data);
 
             }
         });
